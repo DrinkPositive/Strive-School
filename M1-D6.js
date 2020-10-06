d7eec202cdca873e6cc83118a7aeea3ae3b6aa36
DrinkPositive <59743989+DrinkPositive@users.noreply.github.com>
2020-10-06 19:01:30 +0200

    Add files via upload

---
+++ M1-D6.js
@@ -0,0 +1,68 @@
+<html>
+  <style>
+body {
+  margin:0;
+
+}
+th {
+      text-align: left !important;
+    }
+
+.red-color {
+  color:red;
+    }
+nav {
+  background:#F4F4F4;
+}
+.todo ul{
+    list-style-type: none;
+    padding-inline-start:0px;!important;
+}
+
+nav h1 {
+  margin-left: 50px;
+  padding:30px;
+}
+
+.todo {
+      display: flex;
+      flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+.hidden {
+  display:none;
+}
+
+tr {
+  border: 1px solid #000;
+}
+
+.container {
+      display: flex;
+      flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+table {
+        display: flex;
+      flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+.submit {
+  margin-bottom:10px;
+}
+
+form input {
+      width: 100%;
+}
+
+  </style>
+
+  <title>Strive School - Shop</title>
+
+</html>
