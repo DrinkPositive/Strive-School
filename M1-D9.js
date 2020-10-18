e436dd93ea020bd372ab7114e82c4b57dfb278b5
MakZ <59743989+MakZ@users.noreply.github.com>
2020-10-18 13:40:28 +0200

    Update M1-D9.js

--- M1-D9.js
+++ M1-D9.js
@@ -1,35 +1,35 @@
 <!DOCTYPE html>
 <html>
-<style>
-  .container {
-    display: flex;
-  }
+  <style>
+    .container {
+      display: flex;
+    }
 
-  .table {
-    border-left: 1px solid #000000;
-    border-top: 1px solid #000000;
-  }
+    .table {
+      border-left: 1px solid #000000;
+      border-top: 1px solid #000000;
+    }
 
-  .table:nth-child(5) {
-    border-right: 1px solid #000000;
-  }
+    .table:nth-child(5) {
+      border-right: 1px solid #000000;
+    }
 
-  .cell {
-    width: 50px;
-    height: 50px;
-    border-bottom: 1px solid #000000;
-    justify-content: center;
-    align-items: center;
-    display: flex;
-  }
+    .cell {
+      width: 50px;
+      height: 50px;
+      border-bottom: 1px solid #000000;
+      justify-content: center;
+      align-items: center;
+      display: flex;
+    }
 
-  .btn-col {
-    text-align: center;
-    width: 250px;
-    margin-bottom: 10px;
-  }
-</style>
-<!--
+    .btn-col {
+      text-align: center;
+      width: 250px;
+      margin-bottom: 10px;
+    }
+  </style>
+  <!--
         BINGO CHALLENGE!!!
         Create programmatically (with JS) a BINGO board with 76 cells, from 1 to 76.
         Create a button to randomize a number from 1 to 76. The same number should be highlighted on the bingo board
@@ -39,96 +39,74 @@
         - Let the user choose HOW MANY user boards he's willing to play with and create them.
     -->
 
-<body>
-  <div class="btn-col">
-    <button id="btn">Button</button>
-  </div>
-  <div class="container">
-    <div class="table">
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
+  <body>
+    <div class="btn-col">
+      <button id="btn">Button</button>
     </div>
+    <div class="container">
+      <div class="table">
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+      </div>
 
-    <div class="table">
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
+      <div class="table">
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+      </div>
+
+      <div class="table">
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+      </div>
+
+      <div class="table">
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+      </div>
+
+      <div class="table">
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+      </div>
     </div>
+  </body>
 
-    <div class="table">
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-    </div>
+  <script>
+    const btn = document.getElementById("btn");
 
-    <div class="table">
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-    </div>
+    btn.addEventListener("click", () => {
+      for (var i = 0, numbers = []; i < 25; i++) {
+        numbers[i] = i;
+        numbers.sort(function () {
+          return Math.random() - 0.5;
+          Console.log(numbers);
+        });
+      }
 
-    <div class="table">
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-      <div class="cell"></div>
-    </div>
-  </div>
-
-</body>
-
-<script>
-
-
-
-
-  const btn = document.getElementById("btn");
-
-
-
-
-  btn.addEventListener("click", () => {
-    for (var i = 0, numbers = []; i < 25; i++) {
-          numbers[i] = i;
-          numbers.sort(function () {
-        return Math.random() - 0.5;
-      });
-    }
-    number1 = numbers[1];
-    number2 = numbers[2];
-    number3 = numbers[3];
-    number4 = numbers[4];
-    number5 = numbers[5];
-
-    for (var i = 0; i < 25; i++) {
-      secondDiv = document.createElement("div");
-      selectDivContainer = document.querySelectorAll('.cell');
-      selectDivContainer[i].style.background = ("white");
-      selectDivContainer[i].innerText = numbers[i];
-
-    }
-
-    selectDivContainer[number1].style.background = ("green");
-    selectDivContainer[number2].style.background = ("green");
-    selectDivContainer[number3].style.background = ("green");
-    selectDivContainer[number4].style.background = ("green");
-    selectDivContainer[number5].style.background = ("green");
-
-
-  });
-
-
-</script>
-
-
+      for (var i = 0; i < 25; i++) {
+        secondDiv = document.createElement("div");
+        selectDivContainer = document.querySelectorAll(".cell");
+        selectDivContainer[i].style.background = "white";
+        selectDivContainer[i].innerText = numbers[i];
+        for (let i = 0; i < 5; i++) {
+          selectDivContainer[numbers[i]].style.background = "green";
+        }
+      }
+    });
+  </script>
 </html>
